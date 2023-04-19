27d7bf5b749637f0d147b3cb62762526bb5b1dbe
lipatovaelen@yandex.ru <lipatovaelen@yandex.ru>
2023-04-19 22:52:27 +0300

    DZ_distans

--- DZ_OOP_1/DZ_OOP_1/main.cpp
+++ DZ_OOP_1/DZ_OOP_1/main.cpp
@@ -1,59 +1,115 @@
-#include <iostream>
-#include <math.h>
-
+//Домашняя работа Липатова Е.
+//группа SPD_211
+//Написать функцию ??? distance(???), которая возвращает расстояние между двумя точками
+#include<iostream>
 using namespace std;
-//не работает??
-/*
+
+#define delimiter "\n--------------------------------------------\n"
+
+
+
 class Point
 {
-    double x, y;
+	double x;
+	double y;
 public:
-    double getx()
-    {
-        return x;
-    }
-    double gety()
-    {
-        return y;
-    }
-    void setx(double value) // аргумент функции
-    {
-        x = value;
-    }
-    void sety(double value)
-    {
-        y = value;
-    }
-    Point(double first, double second)
-    {
-        x = first;
-        y = second;
-    }
-    double length(Point b)
-    {
-        return sqrt((x - b.x) * (x - b.x) + (y - b.y) * (y - b.y));
-    }
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+	Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(double x)
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "1ArgConstructor:\t" << this << endl;
+	}
+	Point(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+				
+
+	//				метод
+	double distance(const Point& other)const//Point other - другой объект (другая точка)
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
+
+	void print()const
+	{
+		cout << "X = " << x << "\tY = " << y << endl;
+	}
 };
 
-*/
-
-// не работает тоже
-class Point
+double distance(const Point& A, const Point& B)
 {
-public:
-    double x;
-    double y;
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+	return distance;
+}
 
-    Point(double xx, double yy)
-    {
-        x = xx; y = yy;
-        cout << "Точка (x, y)..." << x << ", " << y << endl;
-    }
+#define DISTANCE_CHECK
 
-    double distance(double xx, double yy)
-    {
-        double Line = sqrt((x - xx) * (x - xx) + (y - yy) * (y - yy));
-        return Line;
-    }
-};
+void main()
+{
+	setlocale(LC_ALL, "");
+
+
+
+#ifdef DISTANCE_CHECK
+	Point A(2, 3);
+	A.print();
+
+	Point B(7, 8);
+	B.print();
+	cout << delimiter << endl;
+	cout << "Расстояние от точки A до точки B: " << A.distance(B) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние от точки A до точки B: " << B.distance(A) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние между точками A и B:   " << distance(A, B) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние между точками B и A:   " << distance(B, A) << endl;
+	cout << delimiter << endl;
+#endif // DISTANCE_CHECK
+
+
+}
+
+
+
+
 
--- IntroductionToOOP/IntroductionToOOP/main.cpp
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include<iostream>
 using namespace std;
 
+#define delimeter "\n------------------------------\n"
+
 class  Point
 {
 	
@@ -47,11 +49,29 @@ public:
 		cout << "Constructor:\t\t" << this << endl;
 	}
 
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor: \t " << this << endl;
+	}
+
 
 	~Point()
 	{
 		cout << "Destructor:\t\t" << this << endl;
 	}
+	
+	// оператор
+	Point& operator =(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
+	}
+
+
 
 	//metod
 
@@ -60,11 +80,36 @@ public:
 		cout << "X = " << x << "\ty = " << y << endl;
 	}
 
+	double distance(Point& other)const //Point other - другой объект другая точка
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+
+		double distance = sqrt(x_distance* x_distance + y_distance * y_distance);
+		return distance;
+	}
+
+	
+
 	
 };
 
+double distance(const Point& A, const Point& B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+	return distance;
+}
+
+
 //#define STRUCT_POINT
 //#define CONSTRSTRUCT_CHEK
+//#define DISTANCE_CHECK
+//#define ASSIGNMENT_CHEK
+
+
+
 
 void main()
 {
@@ -84,7 +129,6 @@ void main()
 
 #endif // STRUCT_POINT
 	
-	
 #ifdef CONSTRSTRUCT_CHEK
 	Point A;
 	//A.set_x(2);
@@ -92,12 +136,63 @@ void main()
 	//cout << A.get_x() << "\t" << A.get_y() << endl;
 	A.print();
 
-	Point B = 5;
+	Point B = 5; //Single - argument constructot
 	B.print();
 
 	Point C(2, 3);
 	C.print();
+
+	Point D = C; //конструктор копирвоания  new
+
+	Point E;
+	E = D;  //оператор присваивания
+
+
+
 #endif // CONSTRSTRUCT_CHEK
+#ifdef DISTANCE_CHECK
 
 
+
+	Point A(2, 3);
+	A.print();
+
+	Point B(7, 8);
+	B.print();
+	cout << "Расстояние от точки А до точки В: " << A.distance(B) << endl;
+	cout << delimeter << endl;
+	cout << "Расстояние от точки B до точки A: " << B.distance(A) << endl;
+	cout << delimeter << endl;
+	cout << "Расстояние между точками   A и В: " << distance(A, B) << endl;
+	cout << delimeter << endl;
+	cout << "Расстояние между точками   B и A: " << distance(B, A) << endl;
+
+#endif // DISTANCE_CHECK
+#ifdef ASSIGNMENT_CHEK
+
+	int a, b, c;
+	a = b = c = 0;
+	cout << a << "\t" << b << "\t" << c << endl;
+
+	Point A, B, C;
+	cout << delimeter << endl;
+	A = B = C = Point(2, 3);
+	cout << delimeter << endl;
+	A.print();
+	B.print();
+	C.print();
+#endif // ASSIGNMENT_CHEK
+
+
+	//операторы перегрузка
+	/*1 перегрузка только существующдих операторов
+	+ перегружается
+	++ перегружается
+	%% не перегружается
+	@ не перегружается
+	2, не все иперегружаются
+	3, перегруженные сохраняют приоритет
+	4, изменить поаведение операторов надо свтроенными типами невозвожно
+
+*/
 };
